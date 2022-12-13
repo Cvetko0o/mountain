78ddba95922276fc80d6674b51f699862b3d0363
Cvetko0o <55314876+Cvetko0o@users.noreply.github.com>
2022-12-14 00:48:20 +0200

    data update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import Navbar from './Navbar'
 import Main from './Main'
+import data from './data'
+
 
 function App() {
+
+  const dataTravel = data.map(el => {
+    return (<Main
+      title= {el.title}
+      location={el.location}
+      googleMapsUrl="https://goo.gl/maps/1DGM5WrWnATgkSNB8"
+      startDate={el.startDate}
+      endDate={el.endDate}
+      description={el.description}
+      imageUrl={el.imageUrl}
+    />)
+  })
+
   return (
     <div>
       <Navbar />
-      <Main />
-      <Main />
-      <Main />
-
+      {dataTravel}
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -2,17 +2,18 @@
 
 export default function Main(props) {
     console.log(props)
+    
     return (
         <div>
-            <img className="googleMapsUrl" src="https://goo.gl/maps/1DGM5WrWnATgkSNB8" alt=""/>
-            <h3 className="location">{props.location}</h3>
+            <img src="https://goo.gl/maps/1DGM5WrWnATgkSNB8" alt=""/>
+            <h3>{props.location}</h3>
             <p>View on Google Maps</p>
-            <h2 className="title">Mount Fuji</h2>
-            <img className="imageUrl" src="https://source.unsplash.com/WLxQvbMyfas" alt="" width="200px" />
-            <p className="startDate">12 Jan, 2021</p>
+            <h2>{props.title}</h2>
+            <img src={props.imageUrl} alt="" width="200px" />
+            <p>{props.startDate}</p>
             <p> - </p>
-            <p className="endDate">24 Jan, 2021</p>
-            <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+            <p>{props.endDate}</p>
+            <p>{props.description}</p>
         </div>
     )
 }

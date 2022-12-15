6d379564a88b86dbbaf91649e4257a76652822f5
Cvetko0o <55314876+Cvetko0o@users.noreply.github.com>
2022-12-15 22:46:04 +0200

    styling

--- src/Main.js
+++ src/Main.js
@@ -4,15 +4,15 @@ export default function Main(props) {
     console.log(props)
     
     return (
-        <div>
-            <img src="https://goo.gl/maps/1DGM5WrWnATgkSNB8" alt=""/>
-            <h3>{props.location}</h3>
-            <p>View on Google Maps</p>
-            <h2>{props.title}</h2>
-            <img src={props.imageUrl} alt="" width="200px" />
-            <p>{props.startDate}</p>
-            <p> - </p>
-            <p>{props.endDate}</p>
+        <div className="main">
+            <img src="https://img.freepik.com/free-vector/location_53876-25530.jpg?w=2000" alt="" className="image--icon"/>
+            <h3 className="location">{props.location}</h3>
+            <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8" className="maps">View on Google Maps</a>
+            <h2 className="title">{props.title}</h2>
+            <img src={props.imageUrl} alt="" className="image--address" />
+            <p className="startDate">{props.startDate}</p>
+            <p className="dash--date"> - </p>
+            <p className="endDate">{props.endDate}</p>
             <p>{props.description}</p>
         </div>
     )
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/download.png" className="img--logo" />
+            <img src="../images/download.png" className="img--logo" alt=""/>
             <p className="logo--txt">my travel journal</p>
         </nav>
     )
